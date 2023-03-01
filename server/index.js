02ffd3a0944dfffd4ded968dcdd01da9f9b5425b
AhmadAssal <ahmadnaderassal@gmail.com>
2023-03-01 10:32:15 +0200

    fix: fix CORS issue

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const socketIO = require("socket.io");
 const http = require("http");
+const cors = require("cors");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 3333;
 const router = require("./router");
 const app = express();
+
+app.use(cors());
+
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIO(server, { cors: { origin: "http://localhost:3000" } });
 
 io.on("connection", (socket) => {
   console.log("We have a new connection!");
