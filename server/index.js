7cdce86f36b62a71e8f54ae309a80d763ab149a4
AhmadAssal <ahmadnaderassal@gmail.com>
2023-03-02 14:23:38 +0200

    feat: implement server socket code

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const socketIO = require("socket.io");
 const http = require("http");
 const cors = require("cors");
 
+const { addUser, getUser, removeUser, getUsersInRoom } = require("./users");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -16,10 +18,36 @@ const server = http.createServer(app);
 const io = socketIO(server, { cors: { origin: "http://localhost:3000" } });
 
 io.on("connection", (socket) => {
-  console.log("We have a new connection!");
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
+    if (!name) return;
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name} joined room ${user.room}`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} joined!` });
+
+    socket.join(user.room);
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
+  });
   socket.on("disconnect", () => {
-    console.log("User has disconnected");
+    const user = removeUser(socket.id);
+    if (user[0]) {
+      io.to(user[0].room).emit("message", {
+        user: "admin",
+        text: `${user[0].name} left.`,
+      });
+    }
   });
 });
 
